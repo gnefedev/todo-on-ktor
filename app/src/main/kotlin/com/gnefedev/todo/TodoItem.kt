64bff40d4602685c301c905f4c73099515d08490
gnefedev <georgynefedev@gmail.com>
2022-01-09 14:21:09 +0300

    date

--- app/src/main/kotlin/com/gnefedev/todo/TodoItem.kt
+++ app/src/main/kotlin/com/gnefedev/todo/TodoItem.kt
@@ -5,7 +5,10 @@ import java.util.*
 data class TodoItem(
     val id: Id,
     val message: String,
-    val status: Status = Status.OPEN
+    val status: Status = Status.OPEN,
+//    val date: Instant
+//        Instant.now().plus(Duration.ofDays(1))
+
 ) {
     data class Id(
         val uniq: UUID
